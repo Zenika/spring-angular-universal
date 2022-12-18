3f33253835072a4ae823a3d3b8beaa535cdc236f
Martin Choraine <martin.choraine@gmail.com>
2022-12-18 14:31:24 +0100

    init

---
+++ src/main/kotlin/com/mchoraine/springangularuniversal/SpringAngularUniversalApplication.kt
@@ -0,0 +1,11 @@
+package com.mchoraine.springangularuniversal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAngularUniversalApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringAngularUniversalApplication>(*args)
+}
---
+++ src/test/kotlin/com/mchoraine/springangularuniversal/SpringAngularUniversalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mchoraine.springangularuniversal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAngularUniversalApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
